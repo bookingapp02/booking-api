fcb4e040f97cd7a1249e1df7f7b25d988a96ad1f
Andrei Sandica <andrei.sandica.94@gmail.com>
2023-11-14 23:25:37 +0200

    Test tag 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! From other app version! 0,0.7' + process?.env?.DB_URL;
+    return 'Hello World! From other app version! 0,0.8, Github action added';
   }
 }
